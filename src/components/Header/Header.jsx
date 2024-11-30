c9bf004fa2b33c2546bf92c1bf6635cab177ca23
petrushevskiyMaksim <maxinverst@gmail.com>
2024-11-30 20:05:22 +0500

    add title,body,user, but no img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,8 @@ import Contacts from '../Contacts/Contacts';
 import Admin from '../Admin/Admin';
 
 export default function Header() {
-	
 	return (
-		<header className='flex justify-between items-center font-bold h-24 mb-7'>
+		<header className='flex justify-between items-center font-bold h-24 mb-2'>
 			<Logo text={'Fox News'} />
 			<Contacts phone={'(348)981.872'} email={'hello@foxnews.com'} />
 			<Admin />
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
-const urlUsers = 'https://jsonplaceholder.typicode.com/users';
-
 export default function Main({ title }) {
-	const [posts, setPosts] = useState(null);
+	const [posts, setPosts] = useState([]);
+	const [users, setUsers] = useState([]);
 
 	async function getPosts() {
 		const urlPosts = 'https://jsonplaceholder.typicode.com/posts';
@@ -17,29 +16,71 @@ export default function Main({ title }) {
 		}
 	}
 
+	async function getUsers() {
+		const urlUsers = 'https://jsonplaceholder.typicode.com/users';
+
+		try {
+			const response = await fetch(urlUsers);
+			const data = await response.json();
+			console.log(data);
+			setUsers(data);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	useEffect(() => {
+		getUsers();
+	}, []);
+
 	useEffect(() => {
 		getPosts();
 	}, []);
-	console.log(posts);
-	// {
-	// 	const list = posts.map(post => <li>{post.title}</li>);
-	// }
+
+	const getUserById = userId => {
+		return users.find(user => user.id === userId);
+	};
+
 	return (
-		posts && (
-			<main>
-				<h1 className='after:content-[""] after:inline-block after:w-64 after:bg-black  after:h-0.5  flex flex-col gap-y-2 items-center  text-4xl font-bold  text-center mb-7'>
-					{title}
-				</h1>
+		<main>
+			<h1 className='after:content-[""] after:inline-block after:w-64 after:bg-black  after:h-0.5  flex flex-col gap-y-2 items-center  text-4xl font-bold  text-center mb-14'>
+				{title}
+			</h1>
+			{posts && (
 				<section>
 					<div>
-						<ul>
-							{posts.map(post => (
-								<li key={post.id}>{post.title}</li>
-							))}
+						<ul className='list'>
+							{posts.map(post => {
+								const user = getUserById(post.id);
+
+								return (
+									<li key={post.id} className='list__item'>
+										<h2 className='text-2xl font-bold capitalize'>
+											{post.title}
+										</h2>
+										<p className='first-letter:uppercase'>{post.body}</p>
+										{user && (
+											<div className='flex items-center gap-x-3'>
+												<img
+													className='max-w-14 rounded-2xl'
+													src='src/assets/user-image-1.jpg'
+													alt=''
+												/>
+												<div>
+													<p className='font-medium'>
+														<strong>{user.name}</strong>
+													</p>
+													<p className='text-sm opacity-80'>{user.username}</p>
+												</div>
+											</div>
+										)}
+									</li>
+								);
+							})}
 						</ul>
 					</div>
 				</section>
-			</main>
-		)
+			)}
+		</main>
 	);
 }
