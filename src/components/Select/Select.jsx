c8d91d446f15d95107ecc8b046fde94e3a622fd8
petrushevskiyMaksim <maxinverst@gmail.com>
2025-01-23 19:35:16 +0500

    change select and option

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { HiChevronUpDown } from 'react-icons/hi2';
+import './select.css';
 
 export default function Select({ users, onChange }) {
 	return (
-		<select onChange={onChange}>
-			<option value={'all'}>All Users</option>
-			{users.map(user => (
-				<option key={user.id} value={user.id}>
-					{user.name}
+		<div className='wrapper-select'>
+			<select className='' onChange={onChange}>
+				<option className='my-option' value={'all'}>
+					<div>All Users</div>
 				</option>
-			))}
-		</select>
+				{users.map(user => (
+					<option className='my-option' key={user.id} value={user.id}>
+						{user.name}
+					</option>
+				))}
+			</select>
+			<HiChevronUpDown className='icon' />
+		</div>
 	);
 }
